80b5db812fefe080a68c78e5283b401c80b4a997
Masrura <masrura.uddin96@gmail.com>
2021-12-30 23:32:39 +0600

    movies added

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bjnos.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://mydbuser1:Masi_moni13@cluster0.bjnos.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 console.log('uri', uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
@@ -19,7 +19,38 @@ async function run() {
         await client.connect();
         console.log("connected to db");
         const database = client.db("boost_challenge");
+        const mainCollection = database.collection("main_collection");
+        const watchlistCollection = database.collection("watchlist");
 
+        app.get('/movies', async (req, res) => {
+            const cursor = mainCollection.find({});
+            const movies = await cursor.toArray();
+            res.send(movies);
+        })
+
+        app.get('/movie/:mId', async (req, res) => {
+            const id = req.params.mId;
+            console.log(id);
+            const query = { _id: ObjectId(id) };
+            const result = await mainCollection.findOne(query);
+            res.send(result);
+        });
+
+        app.post('/watchlist', async (req, res) => {
+            const order = req.body;
+            const result = await watchlistCollection.insertOne(order);
+            res.json(result);
+        });
+        app.get('/watchlist/:email', async (req, res) => {
+            const email = req.params.email;
+            console.log(email);
+            const query = { email: email };
+            const cursor = await watchlistCollection.find(query);
+            const result = await cursor.toArray();
+            console.log(result);
+            res.json(result);
+
+        });
     }
     finally {
         //await client.close();

70bfb857688776632370b130e41fe81b95f437fb
Masrura <masrura.uddin96@gmail.com>
2022-01-02 03:05:08 +0600

    functionality

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ async function run() {
         const database = client.db("boost_challenge");
         const mainCollection = database.collection("main_collection");
         const watchlistCollection = database.collection("watchlist");
+        const usersCollection = database.collection("users");
 
         app.get('/movies', async (req, res) => {
             const cursor = mainCollection.find({});
@@ -28,9 +29,15 @@ async function run() {
             res.send(movies);
         })
 
+        app.post('/movies', async (req, res) => {
+            const order = req.body;
+            const result = await mainCollection.insertOne(order);
+            res.json(result);
+        });
+
         app.get('/movie/:mId', async (req, res) => {
             const id = req.params.mId;
-            console.log(id);
+           // console.log(id);
             const query = { _id: ObjectId(id) };
             const result = await mainCollection.findOne(query);
             res.send(result);
@@ -44,13 +51,58 @@ async function run() {
         app.get('/watchlist/:email', async (req, res) => {
             const email = req.params.email;
             console.log(email);
-            const query = { email: email };
+            const query = { user: email };
             const cursor = await watchlistCollection.find(query);
             const result = await cursor.toArray();
-            console.log(result);
-            res.json(result);
+            console.log('result is', result);
+            const watchlist=[];
+            for (r of result) {
+                console.log('id is', r.movieId);
+                const query2 = { _id: ObjectId(r.movieId) };
+                const result2 = await mainCollection.findOne(query2);
+                watchlist.push(result2);
+            }
+           // console.log('watchlist is ', watchlist)
+            res.json(watchlist);
 
         });
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            // console.log(req.body);
+            const result = await usersCollection.insertOne(user);
+            console.log('user',result);
+            res.json(result);
+        });
+        app.put('/users', async (req, res) => {
+            const user = req.body;
+            const filter = { email: user.email }
+            const options = { upsert: true };
+            const updateDoc = { $set: user }
+            const result = await usersCollection.updateOne(filter, updateDoc, options);
+            res.json(result)
+        });
+
+        app.get('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            let isAdmin = false;
+            console.log('role',user?.role);
+            if (user?.role === 'admin') {
+                isAdmin = true;
+            }
+            res.json({ admin: isAdmin });
+        });
+
+        app.put('/users/admin', async (req, res) => {
+            const user = req.body;
+            const filter = { email: user.email };
+            const updateDoc = { $set: { role: 'admin' } };
+            const result = await usersCollection.updateOne(filter, updateDoc);
+            res.json(result);
+        })
+
     }
     finally {
         //await client.close();
